fdba9118e9da963103ac422df21f9fe862033ddc
Nuzul07 <nuzulalief154@gmail.com>
2019-01-11 16:43:06 +0700

    Form kelebihan & AdminController

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/images/{filename}', function ($filename)
+{
+	$path = storage_path('images') . '/' . $filename;
+	$file = File::get($path);
+	$type = File::mimeType($path);
+	$response = Response::make($file);
+	$response->header("Content-Type", $type);
+	return $response;
+});
 
 Route::get('/', function () {
     return view('welcome');
@@ -36,21 +45,21 @@ Route::group(['prefix' => 'admin'] , function(){
 	Route::get('/', 'AdminController@index');
 	Route::get('/kelebihan', 'AdminController@indexkelebihan');
 	Route::get('/kelebihan/add', 'AdminController@addkelebihan');
-	Route::get('/kelebihan/save', 'AdminController@savekelebihan');
+	Route::post('/kelebihan/save', 'AdminController@savekelebihan');
 	Route::get('/kelebihan/edit/{id}', 'AdminController@editkelebihan');
-	Route::get('/kelebihan/update', 'AdminController@updatekelebihan');
+	Route::post('/kelebihan/update', 'AdminController@updatekelebihan');
 	Route::get('/kelebihan/delete/{id}', 'AdminController@deletekelebihan');
 	Route::get('/pelayanan', 'AdminController@indexpelayanan');
 	Route::get('/pelayanan/add', 'AdminController@addpelayanan');
-	Route::get('/pelayanan/save', 'AdminController@savepelayanan');
+	Route::post('/pelayanan/save', 'AdminController@savepelayanan');
 	Route::get('/pelayanan/edit/{id}', 'AdminController@editpelayanan');
-	Route::get('/pelayanan/update', 'AdminController@updatepelayanan');
+	Route::post('/pelayanan/update', 'AdminController@updatepelayanan');
 	Route::get('/pelayanan/delete/{id}', 'AdminController@deletepelayanan');
 	Route::get('/portofolio', 'AdminController@indexportofolio');
 	Route::get('/portofolio/add', 'AdminController@addportofolio');
-	Route::get('/portofolio/save', 'AdminController@saveportofolio');
+	Route::post('/portofolio/save', 'AdminController@saveportofolio');
 	Route::get('/portofolio/edit/{id}', 'AdminController@editportofolio');
-	Route::get('/portofolio/update', 'AdminController@updateportofolio');
+	Route::post('/portofolio/update', 'AdminController@updateportofolio');
 	Route::get('/portofolio/delete/{id}', 'AdminController@deleteportofolio');
 	Route::get('about','AboutController@index');
 	Route::post('about/save','AboutController@save');
