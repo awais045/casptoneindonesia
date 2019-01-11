2e5d8cbe22df37fcbb43067314503e0de616f978
farhan <arramsy0902@gmail.com>
2019-01-11 18:28:45 +0700

    Update Form About

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -35,24 +35,8 @@ About Us
 							<a class="primary-btn" href="#">Get Started Now</a>
 							@endforeach
 						</div>
-						<div class="col-lg-4 col-md-12 home-about-right relative">
-							<form class="form-wrap" action="#">
-								<h4 class="text-white pb-20">Request a Quote</h4>
-								<div class="form-select" id="service-select"">
-									<select>
-										<option value="1">Select Service</option>
-										<option value="2">Service One</option>
-										<option value="3">Service Two</option>
-										<option value="4">Service Three</option>
-										<option value="5">Service Four</option>
-									</select>
-								</div>								
-								<input type="text" class="form-control" placeholder="name">
-								<input type="phone" class="form-control" placeholder="Phone Number">
-								<input type="email" class="form-control" placeholder="Email Address">
-								<textarea name="message" id="" cols="30" rows="5" placeholder="Message" class="form-control"></textarea>
-								<button class="primary-btn">Request Free Quote</button>
-							</form>
+						<div class="col-lg-4 col-md-12 about-right relative">
+							<img src="{{ asset('/images/capstone.jpeg')}}" class="img-fluid" alt="Responsive image">
 						</div>
 					</div>
 				</div>	
