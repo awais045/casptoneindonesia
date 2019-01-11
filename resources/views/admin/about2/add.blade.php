be968053e2b5e4290aa317a9daf8cf9d500d8912
Firman Kurniawan <mrroz3r@gmail.com>
2019-01-11 18:22:55 +0700

    Form Pelayanan

--- resources/views/admin/about2/add.blade.php
+++ resources/views/admin/about2/add.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.app')
 @section('title')
-Edit Pelayanan
+Add About
 @endsection
 @section('content')
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header text-white" style="background-color: #F26E00">Edit Pelayanan</div>
+        <div class="card-header text-white">About</div>
         <div class="card-body">
-         <form action="{{url('admin/pelayanan/save')}}" method="POST" enctype="multipart/form-data">
+         <form action="{{url('admin/kelebihan/save')}}" method="POST" enctype="multipart/form-data">
           <div class="form-group">
             <label for="formGroupExampleInput">Judul</label>
             <input type="text" class="form-control" name="judul" id="formGroupExampleInput" 
-            placeholder="Judul" required value="{{$pelayanan->judul}}">
+            placeholder="Judul" required>
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput">Keterangan</label>
-            <textarea type="text" class="form-control" name="keterangan" id="formGroupExampleInput" 
-            placeholder="Keterangan" required>{{$pelayanan->keterangan}}</textarea>
+            <input type="text" class="form-control" name="keterangan" id="formGroupExampleInput" 
+            placeholder="Keterangan" required>
           </div>
           <div class="form-group">
             <label for="formGroupExampleInput">Gambar</label>
