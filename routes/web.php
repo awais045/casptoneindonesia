9aa09a879efea49b13634eae609487c24d1d1158
Nuzul07 <nuzulalief154@gmail.com>
2019-01-11 16:00:12 +0700

    backend

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,25 @@ Route::get('/contact', function(){
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'] , function(){
+	Route::get('/', 'AdminController@index');
+	Route::get('/kelebihan', 'AdminController@indexkelebihan');
+	Route::get('/kelebihan/add', 'AdminController@addkelebihan');
+	Route::get('/kelebihan/save', 'AdminController@savekelebihan');
+	Route::get('/kelebihan/edit/{id}', 'AdminController@editkelebihan');
+	Route::get('/kelebihan/update', 'AdminController@updatekelebihan');
+	Route::get('/kelebihan/delete/{id}', 'AdminController@deletekelebihan');
+	Route::get('/pelayanan', 'AdminController@indexpelayanan');
+	Route::get('/pelayanan/add', 'AdminController@addpelayanan');
+	Route::get('/pelayanan/save', 'AdminController@savepelayanan');
+	Route::get('/pelayanan/edit/{id}', 'AdminController@editpelayanan');
+	Route::get('/pelayanan/update', 'AdminController@updatepelayanan');
+	Route::get('/pelayanan/delete/{id}', 'AdminController@deletepelayanan');
+	Route::get('/portofolio', 'AdminController@indexportofolio')
+	Route::get('/portofolio/add', 'AdminController@addportofolio');
+	Route::get('/portofolio/save', 'AdminController@saveportofolio');
+	Route::get('/portofolio/edit/{id}', 'AdminController@editportofolio');
+	Route::get('/portofolio/update', 'AdminController@updateportofolio');
+	Route::get('/portofolio/delete/{id}', 'AdminController@deleteportofolio');
+});
