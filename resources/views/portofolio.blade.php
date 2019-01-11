63f1fc4b181b654ce3c9a9270b69bffe5111b59e
geofanytobing <geofany.tobing@yahoo.com>
2019-01-11 18:43:50 +0700

    Update Form PortoFolio UI

--- resources/views/portofolio.blade.php
+++ resources/views/portofolio.blade.php
@@ -9,29 +9,62 @@
                     Who are in extremely love with eco friendly system..
                 </p>
             </div>
-        </div>                      
+        </div>    
+              
         <div class="row">
-            <div class="col-lg-8 col-md-8">
-                <a href="{{ asset ('industry/img/p1.jpg')}}" class="img-gal">
-                    <img class="img-fluid single-project" src="{{ asset ('industry/img/p1.jpg')}}" alt="">
-                </a>    
-            </div>
+             <?php
+              $portofolio = \App\Portofolio::all(); 
+               ?>
+               @foreach($portofolio as $q)
             <div class="col-lg-4 col-md-4">
-                <a href="{{ asset ('industry/img/p2.jpg')}}" class="img-gal">
-                    <img class="img-fluid single-project" src="{{ asset ('industry/img/p2.jpg')}}" alt="">
-                </a>    
-            </div>                      
-            <div class="col-lg-6 col-md-6">
-                <a href="{{ asset ('industry/img/p3.jpg')}}" class="img-gal">
-                    <img class="img-fluid single-project" src="{{ asset ('industry/img/p3.jpg')}}" alt="">
+
+                <a href="#{{$q->id}}" data-toggle="modal" data-traget="#{{$q->id}}" >
+                    <img class="img-fluid" src="{{ url('images/'.$q->gambar)}}" alt="" style="width: 350px; height: 300px;">    
+                    <div class="modal fade" id="{{$q->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Deskripsi</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body"  style="color: black; font-size: 15px;">
+        <div class="text-center">
+        <img src="{{url('images/'.$q->gambar)}}" class="img-fluid" style="width: 466px; height: 250px;">
+        </div>
+        <br>    
+<div class="table-responsive"> 
+<table id="" class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Judul</th>
+      <th scope="col">Keterangan</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>{{$q->judul}}</td>
+      <td>{{$q->keterangan}}</td>
+   
+    </tr>
+  </tbody>
+</table>
+      </div>
+    </div>
+  </div>  
+  </div>  
                 </a>    
             </div>
-            <div class="col-lg-6 col-md-6">
-                <a href="{{ asset ('industry/img/p4.jpg')}}" class="img-gal">
-                    <img class="img-fluid single-project" src="{{ asset ('industry/img/p4.jpg')}}" alt="">
-                </a>    
-            </div>      
-        </div>
+        
+            </div>   
+            <!-- Batas Foreach --> 
+
+             
+             @endforeach
+            </div>
+        
+            </div>  
     </div>  
 </section>
 @endsection
