c496c90d435b893ecd7892ba0db5cfb30e5d7c80
Firman Kurniawan <mrroz3r@gmail.com>
2019-01-14 12:22:02 +0700

    Newsletter & Footer

--- resources/views/admin/pelayanan/edit.blade.php
+++ resources/views/admin/pelayanan/edit.blade.php
@@ -6,22 +6,21 @@ Edit Pelayanan
 <div class="container">
   <div class="row justify-content-center">
     <div class="col-md-8">
-<<<<<<< HEAD
       <hr>
-      <form action="{{url('admin/kelebihan/save')}}" method="POST" enctype="multipart/form-data">
+      <form action="{{url('admin/pelayanan/update')}}" method="POST" enctype="multipart/form-data">
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="judul"></label>
-      <input type="text" class="form-control" name="judul" id="inputEmail4" value="{{$kelebihan->judul}}" placeholder="Judul">
+      <input type="text" class="form-control" name="judul" id="inputEmail4" value="{{$pelayanan->judul}}" placeholder="Judul">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4"></label>
-      <input type="text" class="form-control" name="keterangan" id="inputPassword4" value="{{$kelebihan->keterangan}}" placeholder="Keterangan">
+      <input type="text" class="form-control" name="keterangan" id="inputPassword4" value="{{$pelayanan->keterangan}}" placeholder="Keterangan">
     </div>
   </div>
   <div class="form-group">
     <label for="inputAddress"></label>
-    <input type="file" class="form-control" name="gambar" id="inputAddress" value="{{$kelebihan->gambar}}" placeholder="">
+    <input type="file" class="form-control" name="gambar" id="inputAddress" value="{{$pelayanan->gambar}}" placeholder="">
   </div>
   <br>
   <hr>
