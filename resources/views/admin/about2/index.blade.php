4d93f4e49fcbfb1fe1a7a68e083b085534123a3e
Firman Kurniawan <mrroz3r@gmail.com>
2019-01-14 12:18:38 +0700

    Newsletter & Footer

--- resources/views/admin/about2/index.blade.php
+++ resources/views/admin/about2/index.blade.php
@@ -7,28 +7,27 @@ About
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">About 2</div>
+        <div class="card-header">About Footer</div>
         <div class="card-body">
-         <form action="{{url('admin/about2/update')}}" method="POST" enctype="multipart/form-data"><?php
-              $i= 1;
-              $about2 = \App\About2::all();
-              ?>
-              @foreach($about2 as $q)
+         <form action="{{url('admin/about2/update')}}" method="POST" enctype="multipart/form-data">
           <div class="form-group">
-            <label for="formGroupExampleInput">Judul</label>
-            <input type="text" class="form-control" name="judul" id="formGroupExampleInput" 
-            placeholder="Judul" required value="{{$q->judul}}">
-            <input type="hidden" name="id" value="{{$q->id}}">
+            <label for="formGroupExampleInput">Pengantar 1</label>
+            <input type="text" class="form-control" name="pengantar1" id="formGroupExampleInput" 
+            placeholder="Judul" required value="{{$data->pengantar1}}">
           </div>
           <div class="form-group">
-            <label for="formGroupExampleInput">Deskripsi</label>
-            <input type="text" class="form-control" name="deskripsi" id="formGroupExampleInput" 
-            placeholder="Keterangan" required value="{{$q->deskripsi}}">
+            <label for="formGroupExampleInput">Pengantar 2</label>
+            <input type="text" class="form-control" name="pengantar2" id="formGroupExampleInput" 
+            placeholder="Keterangan" required value="{{$data->pengantar2}}">
+          </div>
+          <div class="form-group">
+            <label for="formGroupExampleInput">Pengantar 3</label>
+            <input type="text" class="form-control" name="pengantar3" id="formGroupExampleInput" 
+            placeholder="Pengarang" required value="{{$data->pengantar3}}">
           </div>
           @csrf
           <button class="btn btn-outline-success float-right" type="submit">Edit Data</button>
           </div>
-          @endforeach
         </form>
       </div>
     </div>
